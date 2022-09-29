2319a6f92d7554e5ecba0463f112314e9d114d17
syahrul25arul <syahrul25arul@gmail.com>
2022-09-29 23:41:55 +0700

    part-2 membuat pengaturan database pooling

---
+++ database.go
@@ -0,0 +1,20 @@
+package go_database
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3307)/belajar_golang_database")
+	if err != nil {
+		panic(err)
+	}
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	return db
+}
--- database_test.go
+++ database_test.go
@@ -1,16 +1,12 @@
 package go_database
 
 import (
-	"database/sql"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func TestOpenConnection(t *testing.T) {
-	db, err := sql.Open("mysql", "root:password@tcp(localhost:3307)/belajar_golang_database")
-	if err != nil {
-		panic(err)
-	}
+	db := GetConnection()
 	defer db.Close()
 }
