242a5f02ffade7bef697ea19ee72c686ebdd5cad
syahrul25arul <syahrul25arul@gmail.com>
2022-09-30 00:20:44 +0700

    part-4 get data dari database

--- sql_test.go
+++ sql_test.go
@@ -23,3 +23,36 @@ func TestExecSql(t *testing.T) {
 
 	fmt.Println("Success insert new customer")
 }
+
+func TestQuerySql(t *testing.T) {
+	// get koneksi database
+	db := GetConnection()
+	// tutup koneksi database di akhir perintah program
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "SELECT id,name FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// tutup rows
+	defer rows.Close()
+
+	// ambil data dari rows
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Id :", id)
+		fmt.Println("Name :", name)
+	}
+
+	fmt.Println("Success insert new customer")
+}
