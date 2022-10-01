f2697ccbf407ea8d775e809bc479103fb7f398c2
syahrul25arul <syahrul25arul@gmail.com>
2022-10-01 22:11:42 +0700

    part-5 tipe data column

--- database_script.sql
+++ database_script.sql
@@ -3,3 +3,23 @@ CREATE TABLE customer (
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY(id)
 )ENGINE = InnoDb;
+
+DELETE FROM customer;
+
+ALTER TABLE customer
+    ADD COLUMN email VARCHAR(100),
+    ADD COLUMN balance INT DEFAULT 0,
+    ADD COLUMN rating DOUBLE DEFAULT 0.0,
+    ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ADD COLUMN birth_date DATE,
+    ADD COLUMN married BOOLEAN DEFAULT false;
+
+DESC customer;
+
+INSERT INTO customer (id,name,email,balance, rating, birth_date, married)
+VALUES ('budi', 'Budi', 'budi@gmail.com', 1000000, 90.0, '1999-10-10', true),
+ ('hendrik', 'Hendrik', 'hendrik@gmail.com', 500000, 85.5, '1995-07-21', false),
+ ('jamal', 'Jamalk', 'jamal@gmail.com', 7500000, 85.5, '1996-06-18', false);
+
+INSERT INTO customer (id,name,email,balance, rating, birth_date, married)
+VALUES ('array', 'Array', NULL, 1000000, 90.0, NULL, true);
--- sql_test.go
+++ sql_test.go
@@ -2,8 +2,10 @@ package go_database
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -52,6 +54,52 @@ func TestQuerySql(t *testing.T) {
 
 		fmt.Println("Id :", id)
 		fmt.Println("Name :", name)
+	}
+
+	fmt.Println("Success insert new customer")
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	// get koneksi database
+	db := GetConnection()
+	// tutup koneksi database di akhir perintah program
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "SELECT id,name,email,balance,rating,birth_date,married,created_at FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// tutup rows
+	defer rows.Close()
+
+	// ambil data dari rows
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balance int32
+		var rating float64
+		var created_at time.Time
+		var birthDate sql.NullTime
+		var married bool
+		err := rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &created_at)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Id :", id)
+		fmt.Println("Name :", name)
+		fmt.Println("Email :", email)
+		fmt.Println("Balance :", balance)
+		fmt.Println("Rating :", rating)
+		fmt.Println("birthDate :", birthDate)
+		fmt.Println("Married :", married)
+		fmt.Println("Created_at :", created_at)
+		fmt.Printf("============\n")
 	}
 
 	fmt.Println("Success insert new customer")
